bb830fe9ea3f882efc4a9f2f2d80c40ba9e7cd96
jacob verhoeks <jacob.verhoeks@gmail.com>
2016-04-06 20:13:11 +0000

    First version of openldap::server::backup that creates crontab backups
    openldap::server::backup { 'dc=test,dc=verhoeks,dc=nl':
        index => 1,
        email => 'root',
    }
    
    openldap::server::backup { 'cn=config':
         index  => 0,
         email => 'root',
     }
    
     Integer $index,
      String[1] $suffix = $title,
      String[1] $hour='*',
      String[1] $minute='5',
      String[1] $weekday='*',
      String[1] $path='/tmp',
      String[1] $email = $::servermonitor

---
+++ manifests/server/backup.pp
@@ -0,0 +1,30 @@
+define openldap::server::backup 
+(
+	Integer $index, 
+	String[1] $suffix = $title,
+	String[1] $hour='*',
+	String[1] $minute='5',
+	String[1] $weekday='*',
+	String[1] $path='/tmp',
+	String[1] $email = $::servermonitor
+)
+{
+    
+ 
+  if ! defined(Class['openldap::server']) {
+        fail 'class ::openldap::server has not been evaluated'
+          }
+
+  $cron_command = "slapcat -b \"${suffix}\"|gzip > \"${path}/openldap-${index}.ldif.gz\""
+  
+  cron { "slapcat-backup-${index}-cron":
+        ensure      => $ensure,
+        command     => $cron_command,
+        user        => $::os::params::adminuser,
+        hour        => $hour,
+        minute      => $minute,
+        weekday     => $weekday,
+        environment => [ 'PATH=/bin:/usr/bin:/usr/sbin', "MAILTO=${email}" ],
+  }
+ 
+}
